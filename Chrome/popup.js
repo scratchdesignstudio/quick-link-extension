c88e8eabaa7271833477938dad817ceb69ec1a77
The_Grits <gritsprogramming@gmail.com>
2016-08-27 23:51:56 -0400

    wip (#1)

---
+++ Chrome/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("mlink").addEventListener("click", go());
+});
+function go(){
+  window.open("https://scratch.mit.edu/"); // change to next project in queue
+}
