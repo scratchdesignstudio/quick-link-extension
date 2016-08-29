cf09d4fe90629b901e3cd93e6507a3a2e911930e
Smrman <vinoa.89.jeux@gmail.com>
2016-08-29 14:07:09 +0200

    Delete popup.js

--- Chrome/popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("mlink").addEventListener("click", go());
-});
-function go(){
-  window.open("https://scratch.mit.edu/"); // change to next project in queue
-}
