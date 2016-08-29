c36cd596e4d2afd855ecbc6753afb5c9d629fc09
The_Grits <gritsprogramming@gmail.com>
2016-08-29 08:00:37 -0400

    Update popup.js

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("mlink").addEventListener("click", go());
+    document.getElementById("linker").addEventListener("click", go());
 });
 function go(){
   window.open("https://scratch.mit.edu/"); // change to next project in queue
